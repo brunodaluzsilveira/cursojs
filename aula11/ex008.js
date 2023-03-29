5cfb99c05fd86f9c89efa08691f2f4586a782b21
brunodaluzsilveira <121897499+brunodaluzsilveira@users.noreply.github.com>
2023-03-29 13:55:03 -0300

    fiz novos exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 68.5
+console.log (`A velocidade do carro é de ${vel}km/h`)
+if (vel>60) {
+    console.log (`Sua velocidade ultrapassou o limite de 60km/h, você foi multado!`)
+}
+console.log ('Dirija sempre com cuidado utilizando o cinto de segurança.')
